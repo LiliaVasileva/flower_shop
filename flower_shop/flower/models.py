f531898aa2fc09a99b9800b89c28a76d695bce82
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-07-18 21:50:18 +0300

    Adding FlowerItem Model and updating settings.py

--- flower_shop/flower/models.py
+++ flower_shop/flower/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class FlowerItem(models.Model):
+    FLOWER_ITEM_NAME_MAX_LENGHT=50
+    item_name = models.CharField(
+        max_length= FLOWER_ITEM_NAME_MAX_LENGHT,
+    )
+    item_picture = models.ImageField(
+        blank=True,
+        null=True
+    )
+    item_price = models.FloatField()
+    item_discription = models.TextField()
+
--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flower_shop.flower',
 ]
 
 MIDDLEWARE = [
@@ -76,9 +77,14 @@ WSGI_APPLICATION = 'flower_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flower_shop',
+        'USER': 'postgres',
+        'PASSWORD': '1123QwER',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 
